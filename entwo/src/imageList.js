1af69a1fc75a7069ff01a6e50d41deaacd809037
Nathan Schmidt <natepsacc@gmail.com>
2022-06-09 15:55:59 -0700

    jun95

--- entwo/src/imageList.js
+++ entwo/src/imageList.js
@@ -76,7 +76,9 @@ const imageClick = (props) => {
                   desktop: '10pt'
                 }
               }}
-               title={item.title} />
+               title={item.title}
+               subtitle={item.author}
+                           />
        </ImageListItem>
      ))}
 
@@ -92,12 +94,14 @@ const itemData = [
     img: 'https://f4.bcbits.com/img/a2212889666_5.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/infinite-castle',
     title: 'infinite-castle',
+    author: 'Indoor Astronauts',
     tag: 'music',
   },
   {
     img: 'https://f4.bcbits.com/img/a0279092000_5.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/track/feel-me',
-    title: 'Feel Me by Thomas Mazzetti',
+    title: 'Feel Me',
+    author:'Thomas Mazzetti',
     tag: 'music',
   },
 
@@ -105,6 +109,31 @@ const itemData = [
     img: 'https://f4.bcbits.com/img/a1335165522_5.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/xanderhill?from=embed',
     title: 'xanderhill',
+    author:'xanderhill',
+    tag: 'music',
+  },
+
+  {
+    img: 'https://f4.bcbits.com/img/a3201002983_5.jpg',
+    mSrc: 'https://eatingnapkins.bandcamp.com/album/dead-at-the-4-20',
+    title: 'dead-at-the-4-20',
+    author:'Pocket Slop',
+    tag: 'music',
+  },
+
+  {
+    img: 'https://f4.bcbits.com/img/a0286543651_5.jpg',
+    mSrc: 'https://eatingnapkins.bandcamp.com/album/songs-for-a-living-corpse',
+    title: 'Songs For A Living Corpse',
+    author:'Wallace Ayers',
+    tag: 'music',
+  },
+
+  {
+    img: 'https://f4.bcbits.com/img/a0842035100_5.jpg',
+    mSrc: 'https://eatingnapkins.bandcamp.com/album/cyclopian-building',
+    title: 'Cyclopian Building',
+    author:'Evan Kallas & Chevy Kalpin',
     tag: 'music',
   },
   {
@@ -112,38 +141,12 @@ const itemData = [
     title: 'Nick at Brain Sweat Practice',
     tag: 'ppl',
   },
-  {
-    img: 'https://f4.bcbits.com/img/a3201002983_5.jpg',
-    mSrc: 'https://eatingnapkins.bandcamp.com/album/dead-at-the-4-20',
-    title: 'dead-at-the-4-20',
-    tag: 'music',
-  },
-
-  {
-    img: 'https://f4.bcbits.com/img/a0286543651_5.jpg',
-    mSrc: 'https://eatingnapkins.bandcamp.com/album/songs-for-a-living-corpse',
-    title: 'Songs For A Living Corpse by Wallace Ayers',
-    tag: 'music',
-  },
-
-  {
-    img: 'https://f4.bcbits.com/img/a0842035100_5.jpg',
-    mSrc: 'https://eatingnapkins.bandcamp.com/album/cyclopian-building',
-    title: 'Cyclopian Building by Chevy Kalpin & Evan Kallas',
-    tag: 'music',
-  },
-
 
   {
     img: 'https://f4.bcbits.com/img/a4239659115_5.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/blood-agar-home',
-    title: 'Blood Agar, Home by Trucking To Tokyo',
-    tag: 'music',
-  },
-  {
-    img: 'https://f4.bcbits.com/img/a0197541223_5.jpg',
-    mSrc: 'https://eatingnapkins.bandcamp.com/album/lich',
-    title: 'LICH by GUTTER HAIR',
+    title: 'Blood Agar, Home',
+    author:'Trucking To Tokyo',
     tag: 'music',
   },
   {
@@ -152,6 +155,14 @@ const itemData = [
     tag: 'ppl',
 
   },
+  {
+    img: 'https://f4.bcbits.com/img/a0197541223_5.jpg',
+    mSrc: 'https://eatingnapkins.bandcamp.com/album/lich',
+    title: 'LICH',
+    author:'GUTTER HAIR',
+    tag: 'music',
+  },
+
 
   {
     img: 'https://i.imgur.com/Ratasd3.jpg',
@@ -163,7 +174,9 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a0736948202_5.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/belac-live-at-the-greenhouse',
-    title: '&#39;belac&#39; live at The Greenhouse by belac',
+    title: '&#39;belac&#39; live at The Greenhouse',
+    author:'Belac',
+    tag: 'live',
     tag: 'music',
   },
   {
@@ -179,7 +192,8 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a3324576764_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/made-to-sink-into-the-ever-curving-mind',
-    title: 'Made to Sink into, The Ever Curving Mind. by Mary MacGuire &amp; Chevy Kalpin',
+    title: 'Made to Sink into, The Ever Curving Mind.',
+    author:'Mary MacGuire & Chevy Kalpin',
     tag: 'music',
   },
 
@@ -187,14 +201,16 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a2466535854_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/porous-joyous-hubris',
-    title: 'Porous Joyous Hubris by Chevy Kalpin',
+    title: 'Porous Joyous Hubris',
+    author:'Chevy Kalpin',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a2576839787_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/track/governed-motif',
-    title: 'Governed Motif by Floor Boy',
+    title: 'Governed Motif',
+    author:'Floor Boy',
     tag: 'music',
   },
 
@@ -203,14 +219,16 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a2466535854_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/booster-pack',
-    title: 'Booster Pack by Pocket Slop',
+    title: 'Booster Pack',
+    author:'Pocket Slop',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a0330761750_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/plainly-lacking-having',
-    title: 'plainly lacking having by Chevy Kalpin',
+    title: 'plainly lacking having',
+    author:'Chevy Kalpin',
     tag: 'music',
   },
 
@@ -218,14 +236,16 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a1377566243_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/crab',
-    title: 'CRAB by Brain Sweat',
+    title: 'CRAB',
+    author:'Brain Sweat',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a3970676027_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/a-brick-fell-out-the-house',
-    title: 'A Brick Fell Out The House by Over The Blue Hills',
+    title: 'A Brick Fell Out The House',
+    author:'Over The Blue Hills',
     tag: 'music',
   },
 
@@ -233,14 +253,16 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a2839535828_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/you-follow-me-everywhere-i-go',
-    title: 'You Follow Me Everywhere I Go by belac',
+    title: 'You Follow Me Everywhere I Go ',
+    author:'belac',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a1357466672_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/track/have-a-sloppy-lil-christmas',
-    title: 'Have a Sloppy lil&#39; Christmas by Pocket Slop',
+    title: 'Have a Sloppy lil&#39; Christmas',
+    author:'Pocket Slop',
     tag: 'music',
   },
 
@@ -248,20 +270,23 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a1914821294_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/dead-horse-sled',
-    title: 'Dead Horse Sled by GUTTER HAIR',
+    title: 'Dead Horse Sled',
+    author:'GUTTER HAIR',
     tag: 'music',
   },
   {
     img: 'https://f4.bcbits.com/img/a2451389976_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/ashita-no-tengu-2',
-    title: 'ashita no tengu by Trucking To Tokyo',
+    title: 'ashita no tengu',
+    author:'Trucking To Tokyo',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a4217347510_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/adultery-seawalls',
-    title: 'Adultery/Seawalls by Floor Boy',
+    title: 'Adultery/Seawalls',
+    author:'Floor Boy',
     tag: 'music',
   },
   {
@@ -274,21 +299,24 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a3073542734_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/how-did-you-end-up-on-your-head',
-    title: 'How did you end up on your head? by Chevy Kalpin',
+    title: 'How did you end up on your head?',
+    author:'Chevy Kalpin',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a3168672433_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/sewer-bald-2',
-    title: 'SEWER BALD by GUTTER HAIR',
+    title: 'SEWER BALD',
+    author:'GUTTER HAIR',
     tag: 'music',
   },
 
   {
     img: 'https://f4.bcbits.com/img/a4114596439_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/thank-you-carpet',
-    title: 'thank you, carpet. by Chevy Kalpin',
+    title: 'thank you, carpet.',
+    author:'Chevy Kalpin',
     tag: 'music',
   },
 
@@ -296,16 +324,13 @@ const itemData = [
   {
     img: 'https://f4.bcbits.com/img/a3782926223_16.jpg',
     mSrc: 'https://eatingnapkins.bandcamp.com/album/live-from-the-cube',
-    title: 'live from the cube by GUTTER HAIR',
+    title: 'live from the cube',
+    author:'GUTTER HAIR',
+    tag: 'live',
     tag: 'music',
   },
 
-  {
-    img: 'https://i.imgur.com/ga6th2f.jpg',
-    title: 'Chevys Night',
-    tag: 'ppl',
 
-  },
 
   {
          img: 'https://i.imgur.com/iFhaZFG.jpg',
